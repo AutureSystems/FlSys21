3cf49f0ee0db0c107f4954f91413b1a5efef7aef
Mossyegghead01 <luthfihaidar1176@gmail.com>
2021-01-12 17:46:30 +0700

    fixed many many many things
    very need testing

--- extensions/help.py
+++ extensions/help.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import math
 
 
 class help(commands.Cog):
@@ -9,7 +10,7 @@ class help(commands.Cog):
     @commands.command()
     async def help(self, ctx, page: int= 1):
         items_per_page = 10
-        p_q = len(player.queue) + 1
+        p_q = len(self.client.commands) + 1
         pages = math.ceil(p_q / items_per_page)
         start = (page - 1) * items_per_page
         end = start + items_per_page
@@ -18,6 +19,7 @@ class help(commands.Cog):
         for index, cmd in enumerate(self.client.commands[start:end], start=start):
             desc += f'fl!{cmd}\n'
         em = discord.Embed(title="Commands", description=desc)
+        em.set_footer(text=f'Viewing page {page}/{pages}')
         await ctx.send(embed=em)
 
 
